c1a366639f79bc108a89f89bfb87c9d82aaf2fd5
Tyler Harms <tyler.harms@gmail.com>
2017-12-05 11:59:54 -0600

    add part 2

--- 2017/day2/corruption-checksum.php
+++ 2017/day2/corruption-checksum.php
@@ -6,9 +6,16 @@ class ChecksumCalculator
 
     private $checksum;
 
+    private $evenDistrobutionSum;
+
     public function getChecksum() : int
     {
         return $this->checksum;
+    }
+
+    public function getEvenDistrobutionSum(): int
+    {
+        return $evenDistrobutionSum;
     }
 
     public function setData(array $dataRows)
@@ -16,24 +23,54 @@ class ChecksumCalculator
         $this->dataRows = $dataRows;
     }
 
-    public function calculate()
+    public function calculateChecksum()
     {
         // always start from zero so that the same object can be used repeatedly without corrupting the data
         $this->checksum = 0;
 
         foreach ($this->dataRows as $dataRow)
         {
-            $this->checksum += $this->calculateRow($dataRow);
+            $this->checksum += $this->calculateCheckumRow($dataRow);
         }
 
         return $this->checksum;
     }
 
-    private function calculateRow(array $data)
+    private function calculateCheckumRow(array $data)
     {
         $min = (int)min($data);
         $max = (int)max($data);
         $diff = $max - $min;
+
+        return $diff;
+    }
+
+    public function calculateEvenlyDistributedSum()
+    {
+        // always start from zero so that the same object can be used repeatedly without corrupting the data
+        $this->evenDistrobutionSum = 0;
+
+        foreach ($this->dataRows as $dataRow) {
+            $this->evenDistrobutionSum += $this->calculateEvenDistributionRow($dataRow);
+        }
+
+        return $this->evenDistrobutionSum;
+    }
+
+    private function calculateEvenDistributionRow(array $data)
+    {
+        $diff = 0;
+
+        foreach ($data as $key => $number) {
+            foreach ($data as $nestedKey => $nestedNumber) {
+                if ($key === $nestedKey) {
+                    continue;
+                }
+                if ((int)$number % (int)$nestedNumber === 0) {
+                    $diff += (int)$number / (int)$nestedNumber;
+                }
+            }
+        }
 
         return $diff;
     }
@@ -50,4 +87,7 @@ fclose($handle);
 $checksumCalculator = new ChecksumCalculator();
 $checksumCalculator->setData($dataRows);
 
-echo $checksumCalculator->calculate() . "\n";
+echo $checksumCalculator->calculateChecksum() . "\n";
+
+echo $checksumCalculator->calculateEvenlyDistributedSum() . "\n";
+
