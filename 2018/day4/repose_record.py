312032925ce260e0662eda572a3b060ca8015d5d
Tyler Harms <tyler.harms@gmail.com>
2018-12-05 14:25:17 -0600

    update to manually calcualte mode and mode factor solves day 4 part 2

--- 2018/day4/repose_record.py
+++ 2018/day4/repose_record.py
@@ -17,16 +17,29 @@ def parse_record(record):
     return {'date': parse_date(date), 'action': action}
 
 
-# https://stackoverflow.com/a/1518632/743245
 def most_common(lst):
-    return max(set(lst), key=lst.count)
+    # return mode(lst)
+    lst = sorted(lst)
+    lst_mode_dict = {}
+    prev_num = -1
+    for item in lst:
+        curr_num = item
+        if curr_num != prev_num:
+            prev_num = curr_num
+            lst_mode_dict.update({prev_num: 1})
+        else:
+            lst_mode_dict.update({curr_num: (lst_mode_dict.get(curr_num) + 1)})
+    mode = max(lst_mode_dict, key=lst_mode_dict.get)
 
+    return {'mode': max(lst_mode_dict, key=lst_mode_dict.get), 'mode_factor': lst_mode_dict.get(mode)}
 
 class Guard:
     def __init__(self, guard_num):
         self.guard_num = guard_num
         self.sleep_minutes = []
         self.sleep_length = 0
+        self.sleep_minutes_mode = 0
+        self.sleep_minutes_mode_factor = 0
 
     def add_sleep(self, minutes):
         minutes_dict = list(minutes)
@@ -65,14 +78,25 @@ for record in records_list:
 sleepiest_guard = Guard('Dummy')
 
 for guard_num, guard in guard_dict.items():
+    if len(guard.sleep_minutes):
+        mode_dict = most_common(guard.sleep_minutes)
+        guard.sleep_minutes_mode = mode_dict.get('mode')
+        guard.sleep_minutes_mode_factor = mode_dict.get('mode_factor')
     if guard.sleep_length > sleepiest_guard.sleep_length:
         sleepiest_guard = guard
+
+most_consistent_sleeper = Guard('Dummy')
+for guard_num, guard in guard_dict.items():
+    if guard.sleep_minutes_mode_factor > most_consistent_sleeper.sleep_minutes_mode_factor:
+        most_consistent_sleeper = guard
 
 
 print("The sleepiest guard #" + sleepiest_guard.guard_num)
 
-most_common_minute = most_common(sleepiest_guard.sleep_minutes)
+most_common_minute = sleepiest_guard.sleep_minutes_mode
 
 print("Most Common Minute: " + str(most_common_minute))
 
 print("Part 1: " + str(most_common_minute * int(sleepiest_guard.guard_num)))
+
+print("Part 2: " + str(most_consistent_sleeper.sleep_minutes_mode * int(most_consistent_sleeper.guard_num)))
